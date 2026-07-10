097c415949c3321e9867f04b0b8fa05cb5fbba47
HP <surajaggarwal62068@gmail.com>
2026-07-10 18:17:42 +0530

    create some sample card

---
+++ 04cardsProject/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="parent">
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+    </div>
+  );
+}
+
+export default App;
